3c1895888d1fac8a7f80d33e85b3fa7e59af5dc9
Fathir <fathirmuhammadarsy1@gmail.com>
2026-02-22 17:15:58 +0700

    update template konfigurasi

--- ui/assets/js/config.example.js
+++ ui/assets/js/config.example.js
@@ -1,9 +1,9 @@
 const CONFIG = {
     // Ganti dengan API Key Gemini kamu (Bisa didapatkan di https://aistudio.google.com/)
-    GEMINI_API_KEY: "YOUR_GEMINI_API_KEY_HERE",
+    GEMINI_API_KEY: "AIzaSyBnSUoONiUiwppnyizdT93PLTiZjnOzWGE",
 
     // Ganti dengan Model Gemini kamu
-    GEMINI_MODEL: "gemini-1.5-flash",
+    GEMINI_MODEL: "gemini-2.5-flash",
 
     // API Endpoint
     GEMINI_API_URL: "https://generativelanguage.googleapis.com/v1beta/models/"
